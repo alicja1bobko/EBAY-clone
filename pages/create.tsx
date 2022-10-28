dfecff0057025512475e74971a2562595dbef81c
AlicjaBobko <alicja_bobko@o2.pl>
2022-10-28 13:22:08 +0200

    feat: add item to marketplace

---
+++ pages/create.tsx
@@ -0,0 +1,14 @@
+import React from "react";
+import Header from "../components/Header";
+
+type Props = {};
+
+const Create = (props: Props) => {
+  return (
+    <div>
+      <Header />
+    </div>
+  );
+};
+
+export default Create;
